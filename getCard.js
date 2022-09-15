aa6241f631543ec621d960bfd422a34e0e806a0b
Sophia Han <eunjookim@sophia-macbook.myfiosgateway.com>
2022-09-15 01:25:39 -0400

    First Commit

---
+++ getCard.js
@@ -0,0 +1,27 @@
+alert("Hello from JS!")
+
+function pick(arr) {
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr(idx);
+}
+
+function getCard() {
+    const values = [
+        '2',
+        '3',
+        '4',
+        '5',
+        '6',
+        '7',
+        '8',
+        '9',
+        '10',
+        'J',
+        'Q',
+        'K',
+        'A'
+    ];
+
+    const suits = ['clubs', 'spades', 'hearts', 'diamonds'];
+    return { value: pick(values), suit: pick(suits) };
+}
